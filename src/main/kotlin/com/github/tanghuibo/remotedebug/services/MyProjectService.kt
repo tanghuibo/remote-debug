e298fb8d80695caad86b9f0cc686b92cf66bbb36
GitHub Action <action@github.com>
2021-11-18 11:39:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/tanghuibo/remotedebug/services/MyProjectService.kt
+++ src/main/kotlin/com/github/tanghuibo/remotedebug/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.tanghuibo.remotedebug.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.tanghuibo.remotedebug.MyBundle
 
 class MyProjectService(project: Project) {
 
